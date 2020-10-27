3f84dcee1073d969edb3abaf4c2932e4cf238951
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-27 16:30:17 +0530

    Refactor the code to take the mood message in construtor

--- src/main/java/com/blz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/blz/moodanalyser/MoodAnalyser.java
@@ -1,18 +1,27 @@
 package com.blz.moodanalyser;
 
 public class MoodAnalyser {
-	private final String message;
+	private String message;
 
-	MoodAnalyser(String message) {
+	public MoodAnalyser() {
+		message = "This is sad message";
+	}
+
+	public MoodAnalyser(String message) {
 		this.message = message;
 	}
 
-	public String analyseMood() {
+	public String analyseMood(String message) {
 		if (message.contains("sad")) {
 			return "sad";
 		} else {
 			return "happy";
 		}
+	}
+
+	public String analyseMood() {
+		this.message = message;
+		return analyseMood(this.message);
 	}
 
 }
