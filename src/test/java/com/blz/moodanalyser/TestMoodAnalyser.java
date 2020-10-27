3c2a4b3974f6e26a9756751eec7995300e692578
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-27 17:28:18 +0530

    Added: Test given null mood should throw exception

--- src/test/java/com/blz/moodanalyser/TestMoodAnalyser.java
+++ src/test/java/com/blz/moodanalyser/TestMoodAnalyser.java
@@ -8,39 +8,92 @@ public class TestMoodAnalyser {
 	@Test
 	public void testGivenMood_WhenSad_ShouldReturnSad() {
 		MoodAnalyser moodAnalyzer = new MoodAnalyser("iam in sad mood");
-		String mood = moodAnalyzer.analyseMood();
-		Assert.assertEquals("sad", mood);
+		String mood = null;
+		try {
+			mood = moodAnalyzer.analyseMood();
+			Assert.assertEquals("sad", mood);
+		} catch (MoodAnalyserException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Test
 	public void testGivenMood_WhenAny_ShouldReturnHappy() {
 		MoodAnalyser moodAnalyzer = new MoodAnalyser("iam in any mood");
-		String mood = moodAnalyzer.analyseMood();
-		Assert.assertEquals("happy", mood);
+		String mood = null;
+		try {
+			mood = moodAnalyzer.analyseMood();
+			Assert.assertEquals("happy", mood);
+		} catch (MoodAnalyserException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	// repeat TC-1.1
 	@Test
 	public void testGivenMessage_NoParameter_ShouldReturnSad() {
-		MoodAnalyser moodAnalyzer = new MoodAnalyser("i am in sad mood");
-		String mood = moodAnalyzer.analyseMood();
-		Assert.assertEquals("sad", mood);
+		MoodAnalyser moodAnalyzer = new MoodAnalyser();
+		String mood = null;
+		try {
+			mood = moodAnalyzer.analyseMood();
+			Assert.assertEquals("sad", mood);
+		} catch (MoodAnalyserException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	// repeat TC-1.2
 	@Test
 	public void givenMessage_NoParameter_ShouldReturnHappy() {
-		MoodAnalyser moodAnalyzer = new MoodAnalyser("i am in happy mood");
-		String mood = moodAnalyzer.analyseMood();
-		Assert.assertEquals("happy", mood);
+		MoodAnalyser moodAnalyzer = new MoodAnalyser();
+		String mood = null;
+		try {
+			mood = moodAnalyzer.analyseMood();
+			Assert.assertEquals("happy", mood);
+		} catch (MoodAnalyserException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	// TC-2.1
 	@Test
 	public void testGivenMessage_WhenNull_ShouldReturnHappy() {
 		MoodAnalyser moodAnalyzer = new MoodAnalyser(null);
-		String mood = moodAnalyzer.analyseMood();
-		Assert.assertEquals("happy", mood);
+		String mood = null;
+		try {
+			mood = moodAnalyzer.analyseMood();
+			Assert.assertEquals("happy", mood);
+		} catch (MoodAnalyserException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	// TC-3.1
+	@Test
+	public void testGivenMood_NullMood_ShouldThrowException() {
+		MoodAnalyser moodAnalyzer = new MoodAnalyser(null);
+		try {
+			String mood = moodAnalyzer.analyseMood();
+		} catch (MoodAnalyserException exception) {
+			Assert.assertEquals("invalid mood", exception.getMessage());
+		}
+	}
+
+	// TC-3.2
+	@Test
+	public void givenMood_EmptyMood_ShouldThrowException() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("");
+		String mood = null;
+		try {
+			mood = moodAnalyser.analyseMood();
+		} catch (MoodAnalyserException exception) {
+			Assert.assertEquals("empty mood", exception.getMessage());
+		}
 	}
 
 }
